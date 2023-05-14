9186d8bcbb88764f4986f4f98423857609a2d64d
unknown <madhavampire@gmail.com>
2023-05-14 09:30:39 +0530

    warspeed

--- app/image_process.py
+++ app/image_process.py
@@ -8,3 +8,15 @@
 def process_image(img):
     print('[*] Image processed successfully')
 
+
+# allowed files
+def allowed_file(filename):
+    return '.' in filename and filename.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']
+
+# get a new filename for a new file uploaded
+number = 0
+def get_new_filename(filename):
+    global number
+    number += 1 
+    return f"image{number}.{filename.split('.')[-1].lower()}"
+

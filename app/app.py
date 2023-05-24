3eeb51616dff695e02505f1886a177120b04eef5
Madhav Kumar <madhavampire@gmail.com>
2023-05-24 11:48:47 +0530

    warspeed

--- app/app.py
+++ app/app.py
@@ -1,47 +1,35 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+""" imports """
 import os
-
 from flask import Flask
 from flask import render_template, redirect, request, url_for, send_file
 # from werkzeug.utils import secure_filename
-
 from PIL import Image
 from utils import *
 
-# ML stuff
-# from detector.engine import detect_faces
 
+""" app """
 app = Flask(__name__)
 
-# start web app
-def start(host="localhost", port=80, debug=True):
-    try:
-        app.run(host=host, port=port, debug=debug)
-    except Exception as err:
-        print("[error]", str(err))
 
-
-""" /routes """
-
-# index page
+""" index page """
 @app.route('/')
 def index():
-    return render_template('old_index.html')
+    return render_template(INDEX)
 
-# upload
+""" upload """
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
         print("[!] File not uploaded")
-        return redirect(request.url)
+        return render_template(INDEX, error_message="File not uploaded!")
     file = request.files['file']
-
     if file.filename == "" or not allowed_file(file.filename):
         print("[!] File not uploaded")
-        return redirect(request.url)
-    
+        return render_template(INDEX, error_message="File not uploaded!")
+
     # save file to the uploads folder
     filename = get_new_filename(file.filename)
     if not os.path.exists('app/uploads'):
@@ -49,44 +37,28 @@ def upload():
     file.save(os.path.join('app/uploads', filename))
     print("[*] File uploaded successfully")
     
-    # # Process the image
-    # img = Image.open(file)
-    # processed_img = process_image(img) # Your image processing code here
-
-    # # Save the processed image to a temporary file
-    # temp_file = 'temp.png'
-    # processed_img.save(temp_file)
+    # Process the image
+    img = Image.open(file)
+    contour_type = process_image(img) # Your image processing code here
 
     # Send the processed image file to the user's browser
-    return send_file(os.path.join('uploads', filename), mimetype='image/png')
+    # return send_file(os.path.join('uploads', filename), mimetype='image/png')
+    return render_template(RESULTS, type=contour_type)
 
 
-# results
+""" results """
 @app.route('/results/<string:type>', methods=['GET'])
 def results(type):
     if type not in ['c1','c2','c3','c4','c5','c6','c7']:
-        return render_template('old_index.html')
+        return render_template(INDEX, error_message="some error occured!")
     lenses = fetch_lenses_from_lenskart(type)
     print("#####################################")
     print(lenses)
     print("#####################################")
-    return render_template('results.html', lenses=lenses)
+    return render_template(RESULTS, lenses=lenses)
 
 
 
-# main
+""" main """
 if __name__ == "__main__":
-    try:
-        host = input("[+] Enter the Host Address: ")
-        port = input("[+] Enter the port for the program: ")
-        debug = False if input("[+] Debug [y/n]: ").lower() in ['no', 'n'] else True
-        start(host=host, port=port, debug=debug)
-
-    except Exception as err:
-        host = "localhost"
-        port = 80
-        print("[error]", str(err))
-        print("[*] Using defaults")
-        print(f"[=] host: {host}")
-        print(f"[=] port: {port}")
-        start(host=host, port=port, debug=debug)
+    start_server(app)
--- app/utils.py
+++ app/utils.py
@@ -3,38 +3,73 @@
 # some imports here
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image
+import uuid
 
+# ML stuff
+# from detector.engine import detect_faces
+
+
+""" constants """
+INDEX = 'old_index.html'
+RESULTS = 'results.html'
 # data
-from data.frames import mapping
-frames = mapping
+mapping = {
+    "c1": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frame_shape_id=11346",
+    "c2": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frametype_id=11371",
+    "c3": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frame_shape_id=11345",
+    "c4": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frame_shape_id=24178",
+    "c5": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frame_shape_id=11351",
+    "c6": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frametype_id=11372",
+    "c7": "https://www.lenskart.com/eyeglasses/promotions/new-arrivals-classic-eyeglasses.html#frametype_id=11371"
+} 
 
-# functions here
+
+""" start web app """
+def start_server(app):
+    try:
+        host = input("[+] Enter the Host Address: ")
+        port = int(input("[+] Enter the port for the program: "))
+        debug = False if input("[+] Debug [y/n]: ").lower() in ['no', 'n'] else True
+    except Exception as err:
+        host = "localhost"
+        port = 80
+        debug = False
+        print("[error]", str(err))
+        print("[*] Using defaults")
+        print(f"[=] host: {host}")
+        print(f"[=] port: {port}")
+    app.run(host=host, port=port, debug=debug)
+
+
+""" functions here """
 def process_image(img):
     print('[*] Image processed successfully')
+    # testing
+    return 'c4'
 
 
-# allowed files
+""" allowed files """
 def allowed_file(filename):
-    return '.' in filename and filename.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']
+    try:
+        return '.' in filename and filename.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']
+    except:
+        return False
 
-# get a new filename for a new file uploaded
-number = 0
+
+""" get a new filename for a new file uploaded """
 def get_new_filename(filename):
-    global number
-    number += 1 
-    return f"image{number}.{filename.split('.')[-1].lower()}"
+    return f"image{str(uuid.uuid1()).replace('-','X')}.{filename.split('.')[-1].lower()}"
 
-# fetching results
+
+""" fetching results """
 def fetch_lenses_from_lenskart(type):
-    response = requests.get(frames[type])
+    response = requests.get(mapping[type])
     soup = BeautifulSoup(response.text, 'html.parser')
     # lenses = [[data['href'], [data['src']]] for data in soup if 'src' in data.attrs and 'href' in data.attrs]
     hrefs = [tag['href'] for tag in soup.find_all('a', href=True)]
     srcs = [tag['src'] for tag in soup.find_all('img', src=True)]
     print(len(hrefs), len(srcs))
-
-
-
     # return lenses 
 
 
